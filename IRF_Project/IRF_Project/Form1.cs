f876f755e69802127175b69b345fa7dfeed9db40
Ákos Demeter <akos.demeter@stud.uni-corvinus.hu>
2020-12-12 17:16:17 +0100

    Szükséges függvények írása

--- IRF_Project/IRF_Project/Form1.cs
+++ IRF_Project/IRF_Project/Form1.cs
@@ -16,15 +16,56 @@ namespace IRF_Project
         IRF_assignmentEntities context = new IRF_assignmentEntities();
         List<TEAM> TEAMs;
         List<GameResult> gameResults = new List<GameResult>();
+        Random rng = new Random(1234); //a seed megadása nem biztos, hogy fog kelleni
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = TEAMs;
         }
+
         private void LoadData() {
             TEAMs = context.TEAMS.ToList();
         }
-        
+
+        //lejátssza az egyes mérkőzéseket
+        private void SimulateMatches() {
+            int numberofteams = TEAMs.Count();
+            for (int i = 0; i < numberofteams; i++)
+            {
+                for (int j = 0; j < numberofteams; j++)
+                {
+                    if (i!=j)
+                    {
+                        
+                    }
+                }
+            }
+        }
+
+        //kiszámolja, hogy az adott meccsen az egyik fél hány gól fog rúgni
+        private void GetGoalsScored(int attacklevel, int midfieldlevel, 
+            int enemydefenselevel, int enemygoalkeeperlevel) {
+
+            int goalsscored = 0;
+            int createdoppotunities = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (rng.NextDouble() <= (midfieldlevel / (midfieldlevel + enemydefenselevel)))
+                {
+                    createdoppotunities = createdoppotunities + 1;
+                }
+            }
+            for (int i = 0; i < createdoppotunities; i++)
+            {
+                if (rng.NextDouble() <= (attacklevel / (attacklevel + enemygoalkeeperlevel)))
+                {
+                    goalsscored = goalsscored + 1;
+                }
+            }
+        }
+
+
     }
 }
