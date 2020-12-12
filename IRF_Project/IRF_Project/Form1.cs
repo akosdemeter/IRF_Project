eda84f6181ea71bd0d1f532d008e6ac64e9993ce
Ákos Demeter <akos.demeter@stud.uni-corvinus.hu>
2020-12-12 16:41:12 +0100

    Adatbetöltés

--- IRF_Project/IRF_Project/Form1.cs
+++ IRF_Project/IRF_Project/Form1.cs
@@ -12,9 +12,16 @@ namespace IRF_Project
 {
     public partial class Form1 : Form
     {
+        IRF_assignmentEntities context = new IRF_assignmentEntities();
+        List<TEAM> TEAMs;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = TEAMs;
+        }
+        private void LoadData() {
+            TEAMs = context.TEAMS.ToList();
         }
     }
 }
